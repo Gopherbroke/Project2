bcd0dc9acef621d2a459512f488b7511c223ca34
Josh Owen <joshalldaylong@gmail.com>
2016-12-15 14:00:30 -0500

    this time with www

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server  = require('http').createServer(app);
-var port    = process.env.PORT || 3000;
+//var port    = process.env.PORT || 3000;
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
 var passport = require('passport');
